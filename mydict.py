1287aa58d98e1e52cedc37f6002b9cc7d156ccf1
rafaeluft <rafaeluft@gmail.com>
2023-06-12 14:24:05 -0300

    Update mydict.py

--- mydict.py
+++ mydict.py
@@ -1,6 +1,6 @@
 def create_dict():
   # Alterar os campos do dicionário sem alterar o nome da variável my_dict
   return {
-      "nome2": "Rafael",
+      "nome": "Rafael",
       "nota": 100.0 
   }
