0e4fe67b17895d28b545ebc297e554a605af6d37
rafaeluft <rafaeluft@gmail.com>
2023-06-12 14:23:13 -0300

    Update mydict.py

--- mydict.py
+++ mydict.py
@@ -1,5 +1,6 @@
 def create_dict():
   # Alterar os campos do dicionário sem alterar o nome da variável my_dict
-  my_dict = {}
-  # FIM do Bloco
-  return my_dict
+  return {
+      "nome2": "Rafael",
+      "nota": 100.0 
+  }
