06f0500eb9808b74c878e6318b5be0841a64a807
rafaeluft <rafaeluft@gmail.com>
2023-06-28 19:09:31 -0300

    Update mydict_test.py

--- mydict_test.py
+++ mydict_test.py
@@ -2,7 +2,7 @@ import mydict;
 
 def test_dict_tem_campos():
   created_dict = mydict.create_dict()
-  assert "nome" in created_dict
+  assert "nome" in created_dict, "O campo nome não consta no dicionário"
   assert "nota" in created_dict
   
 def test_dict_campo_nome_eh_string():
